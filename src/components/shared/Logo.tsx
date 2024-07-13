d6962ae543a7ef1f8a2f32c31254d450919374b0
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-07-13 01:09:27 -0300

    adicionando Link para a Logo

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,5 +1,6 @@
 import logo from '@/assets/logo.svg'
 import logoFooter from '@/assets/logo_footer.svg'
+import { Link } from 'react-router-dom'
 
 interface LogoProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   variant: 'default' | 'footer'
@@ -8,5 +9,9 @@ interface LogoProps extends React.ImgHTMLAttributes<HTMLImageElement> {
 export function Logo({ variant, ...props }: LogoProps) {
   const selectedLogo = variant === 'default' ? logo : logoFooter
 
-  return <img src={selectedLogo} {...props} alt='Digital Store Logo' />
+  return (
+    <Link to='/'>
+      <img src={selectedLogo} {...props} alt='Digital Store Logo' />
+    </Link>
+  )
 }
