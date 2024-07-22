885e3b4bfc1e4e8cafc40123c41fcb3301f2efc4
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-07-22 01:05:54 -0300

    busca funcionando

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 import { ProductItem } from '@/components/shared/ProductItem'
 import { FilterSidebar } from './components/FilterSidebar'
@@ -16,14 +17,24 @@ const options = [
 ]
 
 export function ProductsPage() {
+  const [searchParams] = useSearchParams()
+  const query = searchParams.get('q') || ''
+
   const [products, setProducts] = useState<typeof PRODUCTS | null>(null)
 
   useEffect(() => {
     // Simulando chamada à API
     setTimeout(() => {
-      setProducts(PRODUCTS)
-    }, 800) // delay
-  }, [])
+      if (query) {
+        const filteredProducts = PRODUCTS.filter((p) =>
+          p.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+        )
+        setProducts(filteredProducts)
+      } else {
+        setProducts(PRODUCTS)
+      }
+    }, 650) // delay
+  }, [query])
 
   if (!products) {
     return <ProductsPageSkeleton />
@@ -32,14 +43,16 @@ export function ProductsPage() {
   return (
     <div className='px-5 pt-9 pb-24 md:pb-32 md:px-24 bg-slate-100'>
       <div className='flex flex-col md:flex-row gap-5 md:gap-0 justify-between md:items-center mb-8'>
-        <h1 className='text-zinc-600 order-2 md:order-1'>
-          <span className='text-base font-bold'>
-            Resultados para "Tênis" -{' '}
-          </span>
-          389 produtos
-        </h1>
+        {query && (
+          <h1 className='text-zinc-600 order-2 md:order-1'>
+            <span className='text-base font-bold'>
+              Resultados para "{query}" -{' '}
+            </span>
+            {products.length} produtos
+          </h1>
+        )}
 
-        <div className='order-1 md:order-2 flex gap-2'>
+        <div className='order-1 md:order-2 flex gap-2 self-end'>
           <Dropdown options={options} placeholder='Ordenar por:' />
           <FilterSidebarMobile />
         </div>
