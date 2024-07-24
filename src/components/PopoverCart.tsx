b14075060e5d17b0364344be559822ee0a42f250
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-07-24 10:18:36 -0300

    melhorando o popover de carrinho

--- src/components/PopoverCart.tsx
+++ src/components/PopoverCart.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { ShoppingCartIcon } from 'lucide-react'
 
 import { useCartStore } from '@/stores/useCartStore'
@@ -8,10 +9,22 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover'
 import { cn } from '@/lib/utils'
+import { Button } from './ui/button'
+import { Separator } from './ui/separator'
+import { formatCurrencyBRL } from '@/lib/currency'
 
 export function PopoverCart() {
   const productsInCart = useCartStore((state) => state.productsInCart)
-  const maxVisibleProducts = 5
+  const clearCart = useCartStore((state) => state.clearCart)
+  const maxVisibleProducts = 3
+
+  const calculateTotal = () => {
+    const totalPrice = productsInCart
+      .reduce((total, product) => total + product.price, 0)
+      .toFixed(2)
+
+    return formatCurrencyBRL(Number.parseFloat(totalPrice))
+  }
 
   return (
     <Popover>
@@ -33,46 +46,78 @@ export function PopoverCart() {
           />
         </div>
       </PopoverTrigger>
-      <PopoverContent className='w-80 p-4 mt-2 sm:mr-2'>
-        <div className='grid gap-4'>
-          <div className='space-y-2'>
-            <h4 className='font-medium leading-none'>Carrinho</h4>
-            <p className='text-sm text-muted-foreground'>
-              Produtos adicionados ao carrinho.
-            </p>
+
+      <PopoverContent className='w-80 p-7 mt-2 sm:mr-2'>
+        <div className='grid'>
+          <div>
+            <h4 className='font-bold text-base text-zinc-800 leading-none'>
+              Meu Carrinho
+            </h4>
           </div>
+
+          <Separator className='my-5' />
+
           {productsInCart.length > 0 ? (
-            <div className='grid gap-2'>
-              {productsInCart
-                .slice(0, maxVisibleProducts)
-                .map((product, index) => (
-                  <div
-                    key={index}
-                    className='grid grid-cols-3 items-center gap-4'
-                  >
-                    <div className='col-span-2'>
-                      <p className='text-sm font-medium'>{product.name}</p>
-                      <p className='text-sm text-muted-foreground'>
-                        R${product.price}
-                      </p>
+            <>
+              <div className='grid gap-6'>
+                {productsInCart
+                  .slice(0, maxVisibleProducts)
+                  .map((product, index) => (
+                    <div
+                      key={index}
+                      className='grid grid-cols-3 items-center gap-4'
+                    >
+                      <img
+                        src={product.images[0].url}
+                        alt={product.images[0].altText}
+                        className={cn(
+                          'w-20 h-16 p-2 object-contain rounded',
+                          product.thumbnails[0].bgColor
+                        )}
+                      />
+
+                      <div className='col-span-2'>
+                        <p className='text-sm font-medium'>{product.name}</p>
+                        <p className='text-sm text-muted-foreground'>
+                          R${product.price}
+                        </p>
+                      </div>
                     </div>
-                    <img
-                      src={product.images[0].url}
-                      alt={product.images[0].altText}
-                      className='w-12 h-12 object-cover rounded'
-                    />
-                  </div>
-                ))}
-              {productsInCart.length > maxVisibleProducts && (
-                <p className='text-center text-sm text-muted-foreground'>
-                  E mais {productsInCart.length - maxVisibleProducts} produtos.
-                  <br />
-                  <a href='/carrinho' className='text-primary'>
-                    Ver todos no carrinho
-                  </a>
-                </p>
-              )}
-            </div>
+                  ))}
+                {productsInCart.length > maxVisibleProducts && (
+                  <p className='text-center text-sm text-muted-foreground'>
+                    E mais {productsInCart.length - maxVisibleProducts}{' '}
+                    produtos.
+                    <br />
+                    <Link to='/carrinho' className='text-primary'>
+                      Ver todos no carrinho
+                    </Link>
+                  </p>
+                )}
+              </div>
+
+              <Separator className='my-5' />
+
+              <div className='flex flex-col gap-5'>
+                <div className='flex justify-between font-bold'>
+                  <span className='text-zinc-800'>Valor total:</span>
+
+                  <span className='text-primary'>{calculateTotal()}</span>
+                </div>
+
+                <div className='flex justify-between'>
+                  <Button className='p-0' variant='link' onClick={clearCart}>
+                    Esvaziar
+                  </Button>
+
+                  <Button asChild onClick={clearCart}>
+                    <Link className='hover:text-white' to='/carrinho'>
+                      Ver Carrinho
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            </>
           ) : (
             <p className='text-center text-sm text-muted-foreground'>
               Seu carrinho está vazio.
